b415e9efa32953f2b2b6a4da6ab34b42bb94e89e
Abhinav Parbhakar <abhinav.parbhakar302@gmail.com>
2023-06-01 02:24:52 -0600

    DO NOT INCLUDE THE .PY IN THE STARTING POINT in Setup.py

--- src/arm_control/setup.py
+++ src/arm_control/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "arm_node = arm_control.arm_control_node.py:main"
+            "arm_node = arm_control.arm_control_node:main"
         ],
     },
 )
--- src/teleop/setup.py
+++ src/teleop/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "teleop_node = teleop.joystick.py:main"
+            "teleop_node = teleop.joystick:main"
         ],
     },
 )
