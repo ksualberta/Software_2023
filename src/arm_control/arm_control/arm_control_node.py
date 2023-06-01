374606726f3c91e7648c5add8569302df35adcab
Abhinav Parbhakar <abhinav.parbhakar302@gmail.com>
2023-05-31 23:40:43 -0600

    Added a subscriber to arm_control_node.py

--- src/arm_control/arm_control/arm_control_node.py
+++ src/arm_control/arm_control/arm_control_node.py
@@ -1,2 +1,26 @@
+from rclpy import Node
+from sensor_msgs.msg import Joy
 
+class ArmControl(Node):
+    
+    def __init__(self):
+        super().__init__("armControl_node")
+        self.subcription = self.create_subscription(msg_type=Joy,topic="joystick_node",qos_profile=10,callback = self.armControlCallback)
+
+    def armControlCallback(self,data):
+        self.buttonArray = data.buttons
+        self.axesArray = data.axes
+
+    def getButtonArray(self):
+        return self.buttonArray
+
+    def getAxesArray(self):
+        return self.axesArray
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    armControlNode = ArmControl()
+
+    rclpy.spin(armControlNode)
 
