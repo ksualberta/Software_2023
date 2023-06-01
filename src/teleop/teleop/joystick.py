ff90613bf539a298aace8f3bdc9c76c3169c4de1
Induwara <kandapah@ualberta.ca>
2023-06-01 05:03:24 +0000

    Joystick Publisher Created

--- src/teleop/teleop/joystick.py
+++ src/teleop/teleop/joystick.py
@@ -2,3 +2,10 @@ import rclpy
 from rclpy import Node
 from sensor_msgs import Joy
 
+class JoystickNode(Node):
+    def __init__(self):
+        super().__init__('joystick_node')
+        rclpy.create_node(Joy,'joystick_cmds', 10)
+
+
+        
