b5502a5ba20d96d07b6c121651241c47c3ac1867
Induwara <kandapah@ualberta.ca>
2023-06-02 05:59:27 +0000

    Meshes somework

--- src/description/setup.py
+++ src/description/setup.py
@@ -10,6 +10,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/display.launch.py']),  # Add this line
+        ('share/' + package_name + '/urdf', ['urdf/Arm Full Assembly.urdf']),
+        ('share/' + package_name + '/meshes', ['meshes/base_link.STL', 'meshes/hand_link.STL', 'meshes/link_1.STL', 'meshes/link_2.STL']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
