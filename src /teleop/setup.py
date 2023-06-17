04a8073554a53b53728b0d2bc8c89cb88c7c23c1
= <=>
2023-06-17 22:22:20 +0000

    New teleop

--- src /teleop/setup.py
+++ src /teleop/setup.py
@@ -1,26 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'teleop'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/config', ['joystick.yaml']),
+        ('share/' + package_name + '/launch', ['teleop_launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='indy03',
+    maintainer='Induwara Kandapahala',
     maintainer_email='kandapah@ualberta.ca',
-    description='TODO: Package description',
+    description='Uses Joy Package (ROS Built-in) to get joy inputs and converts to MoveitServo compatiable msgs',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "teleop_node = teleop.joystick:main"
         ],
     },
 )
