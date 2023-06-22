ab9f5eab003f8ea44c98c574202d37243f289f63
= <=>
2023-06-22 17:33:27 +0000

    Error Looking of Panda Arm??

---
+++ src /moveit_config/launch/servo.launch.py
@@ -0,0 +1,53 @@
+import os
+import yaml
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+from launch.actions import ExecuteProcess
+import xacro
+from moveit_configs_utils import MoveItConfigsBuilder
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+def generate_launch_description():
+    
+    moveit_config = (
+        MoveItConfigsBuilder(package_name = "moveit_config", robot_name = "description")
+        .robot_description(file_path="config/description.urdf.xacro")
+        .to_moveit_configs()
+    )
+
+    # Get parameters for the Servo node
+    servo_yaml = load_yaml("moveit_config", "config/moveit_servo_config.yaml")
+    servo_params = {"moveit_config": servo_yaml}
+
+    servo_node = Node(
+        package="moveit_servo",
+        executable="servo_node_main",
+        parameters=[
+            servo_params,
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+        ],
+        output="screen",
+    )
+
+    return LaunchDescription(
+        [
+           
+       
+            servo_node
+    
+        ]
+    )
