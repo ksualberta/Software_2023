f6f498295dcfa1a912c2e51199c502a46f08a778
= <=>
2023-07-11 00:45:08 +0000

    Teleop

--- src /teleop/teleop/Joystick_Arm.py
+++ src /teleop/teleop/Joystick_Arm.py
@@ -11,47 +11,27 @@ Joint_Topic = "/arm_servo/delta_joint_cmds"
 EEF_Frame_ID = "link_5"
 BASE_FRAME_ID = "base_link"
 
-LEFT_STICK_X = 0
-LEFT_STICK_Y = 1
-LEFT_TRIGGER = 2
-RIGHT_STICK_X = 3
-RIGHT_STICK_Y = 4
-RIGHT_TRIGGER = 5
-#D_PAD_X = 6 
-D_PAD_X = 14 #No Clue
-#D_PAD_Y = 7
-D_PAD_Y = 15 #No Clue
 
-A = 0
-B = 1
-#X = 2
-X = 3
-#Y = 3
-Y = 4
-LEFT_BUMPER = 4
-RIGHT_BUMPER = 5
-CHANGE_VIEW = 6
-MENU = 7
-HOME = 8
-#LEFT_STICK_CLICK = 9
-LEFT_STICK_CLICK = 11
-#RIGHT_STICK_CLICK = 10
-RIGHT_STICK_CLICK = 12
+LEFT_STICK_X = 0,
+LEFT_STICK_Y = 1,
+LEFT_TRIGGER = 2,
+RIGHT_STICK_X = 3,
+RIGHT_STICK_Y = 4,
+RIGHT_TRIGGER = 5,
+D_PAD_X = 6,
+D_PAD_Y = 7
 
-
-#PS2 MAPPING 
-
-PS2_DPAD_UP = 13
-PS2_DPAD_DOWN = 14
-PS2_DPAD_LEFT = 15
-PS2_DPAD_RIGHT = 16
-
-PS2_X = 0 
-PS2_CIRCLE = 1
-PS2_SQUARE = 3
-PS2_TRIANGLE = 2
-
-
+A = 0,
+B = 1,
+X = 2,
+Y = 3,
+LEFT_BUMPER = 4,
+RIGHT_BUMPER = 5,
+CHANGE_VIEW = 6,
+MENU = 7,
+HOME = 8,
+LEFT_STICK_CLICK = 9,
+RIGHT_STICK_CLICK = 10
 
 
 Axis_Default = {
@@ -60,9 +40,10 @@ Axis_Default = {
     "RIGHT_TRIGGER" : 1.0,
 }
 
-class JoyToServoPub(Node):
+class joystickArm(Node):
+
     def __init__(self):
-        super().__init__('joy_to_twist_publisher')
+        super().__init__('joystickArm')
         self.frame_to_publish_ = BASE_FRAME_ID
 
         self.twist_pub = self.create_publisher(msg_type = TwistStamped, topic = Twist_Topic, qos_profile = QoSProfile(depth=10))
@@ -78,7 +59,7 @@ class JoyToServoPub(Node):
 
     def JoyMain(self):
 
-        self.UpdateCommandFrame()
+        #self.UpdateCommandFrame()
 
         self.twist_msg = TwistStamped()
         self.joint_msg = JointJog()
@@ -86,38 +67,39 @@ class JoyToServoPub(Node):
         if(self.ConvertJoyToCommand()):
 
             self.twist_msg.header.stamp = self.get_clock().now().to_msg()
-            self.twist_msg.header.frame_id = self.frame_to_publish_
+            self.twist_msg.header.frame_id = "link_5"
             self.twist_pub.publish(self.twist_msg)
 
 
         else:
             
             self.joint_msg.header.stamp = self.get_clock().now().to_msg()
-            self.joint_msg.header.frame_id = self.frame_to_publish_
+            self.joint_msg.header.frame_id = "link_5"
             self.joint_pub.publish(self.joint_msg)
 
 
     def ConvertJoyToCommand(self):
 
-        if(self.joystick_msg.buttons[PS2_CIRCLE] or self.joystick_msg.buttons[PS2_X] or self.joystick_msg.buttons[PS2_TRIANGLE] 
-        or self.joystick_msg.buttons[PS2_SQUARE] or self.joystick_msg.buttons[PS2_DPAD_LEFT] 
-        or self.joystick_msg.buttons[PS2_DPAD_RIGHT] or self.joystick_msg.buttons[PS2_DPAD_UP] or self.joystick_msg.buttons[PS2_DPAD_DOWN]):
+        if(self.joystick_msg.buttons[A] or self.joystick_msg.buttons[B] or self.joystick_msg.buttons[X] 
+        or self.joystick_msg.buttons[Y] or self.joystick_msg.buttons[D_PAD_X] 
+        or self.joystick_msg.buttons[D_PAD_Y]):
 
-            #Shoulder Roll Check 
+
+            #Shoulder Roll 
             self.joint_msg.joint_names.append("Shoulder Roll")
-            self.joint_msg.velocities.append(self.joystick_msg.buttons[PS2_DPAD_RIGHT] - self.joystick_msg.buttons[PS2_DPAD_LEFT])
+            self.joint_msg.velocities.append(self.joystick_msg.axes[D_PAD_X])
             
-            #Shoulder Pitch Check
+            #Shoulder Pitch 
             self.joint_msg.joint_names.append("Shoulder Pitch")
-            self.joint_msg.velocities.append(self.joystick_msg.buttons[PS2_DPAD_UP] - self.joystick_msg.buttons[PS2_DPAD_DOWN])
+            self.joint_msg.velocities.append(self.joystick_msg.axes[D_PAD_Y])
 
-            #Disabled 
+            
             self.joint_msg.joint_names.append("Elbow Roll")
-            self.joint_msg.velocities.append(self.joystick_msg.buttons[PS2_CIRCLE] - self.joystick_msg.buttons[PS2_SQUARE])
+            self.joint_msg.velocities.append(self.joystick_msg.buttons[B] - self.joystick_msg.buttons[X])
 
-            #Wrist Pitch Check
+
             self.joint_msg.joint_names.append("Elbow Pitch")
-            self.joint_msg.velocities.append(self.joystick_msg.buttons[PS2_TRIANGLE] - self.joystick_msg.buttons[PS2_X])
+            self.joint_msg.velocities.append(self.joystick_msg.buttons[Y] - self.joystick_msg.buttons[A])
 
             return False 
 
@@ -143,18 +125,6 @@ class JoyToServoPub(Node):
 
         self.joystick_msg = msg
 
-
-
-    def UpdateCommandFrame(self):
-
-        if (self.joystick_msg.buttons[CHANGE_VIEW] and self.frame_to_publish_ == EEF_Frame_ID):
-            
-                self.frame_to_publish_ = BASE_FRAME_ID
-            
-
-        elif(self.joystick_msg.buttons[MENU] and self.frame_to_publish_ == BASE_FRAME_ID):
-
-            self.frame_to_publish_ = EEF_Frame_ID
 
 
 def main(args=None):
