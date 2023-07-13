9ec5abda76d85ca29ee93c784dbfa5bf70dd7d1f
= <=>
2023-07-13 22:08:12 +0000

    Servo with the additional end effector note broken

--- src /spear_servo/launch/servo.launch.py
+++ src /spear_servo/launch/servo.launch.py
@@ -4,18 +4,29 @@ from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PathJoinSubstitution
+from moveit_configs_utils import MoveItConfigsBuilder
+
+
 import os
 
 param_file = os.path.join(get_package_share_directory("spear_servo"), "config", "moveit_servo_config.yaml")
 
 def generate_launch_description():
 
+    moveit_config = (
+         MoveItConfigsBuilder(robot_name = "", package_name ="spear_servo")
+         .robot_description(file_path="config/SPEAR_Arm.urdf.xacro")
+         .to_moveit_configs()
+     )
+
     ld = LaunchDescription()
     servo_node = Node(
         package="moveit_servo",
         executable="servo_node_main",
         name='arm_servo',
-        parameters= [param_file]
+        parameters= [param_file,
+        moveit_config.robot_description_kinematics],
+        output = 'screen',
         
     )
  
