8f3094d35faeaa5dd0040ebffe76451c9fb5e88b
= <=>
2023-07-13 05:07:54 +0000

    HmMM

---
+++ src /moveit_spear/launch/servo.launch.py
@@ -0,0 +1,23 @@
+
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+import os
+
+param_file = os.path.join(get_package_share_directory("moveit_spear"), "config", "moveit_servo_config.yaml")
+
+def generate_launch_description():
+
+    ld = LaunchDescription()
+    servo_node = Node(
+        package="moveit_servo",
+        executable="servo_node_main",
+        name='arm_servo',
+        parameters= [param_file]
+        
+    )
+ 
+    ld.add_action(servo_node)
+    return ld
