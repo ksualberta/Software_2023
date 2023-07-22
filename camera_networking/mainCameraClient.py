6452a3e4fca2ae58bee6e31f16a44658ba65d2e8
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2023-07-22 17:01:14 -0600

    added split functionality for the data

--- camera_networking/mainCameraClient.py
+++ camera_networking/mainCameraClient.py
@@ -16,6 +16,8 @@ DISMES = '!END' ## Message to disconnect from server
 JPEGQUALITY = 100 ## Quality of image outgoing 0-100
 ENCODEPARAM = [int(cv2.IMWRITE_JPEG_QUALITY), JPEGQUALITY]
 
+SPLIT_RATE = 10
+
 SD  = (480  , 640 )
 HD  = (720  , 1280)
 FHD = (1080 , 1920) ## STANDARD MONITOR
@@ -39,8 +41,8 @@ def start():
     except Exception as ext:
         print('[CLIENT] ERROR, DISCONNECTING')
         print(ext)
-        sendData(client, 'ERROR OCURRED')
-        sendData(client, DISMES)
+        #sendData(client, 'ERROR OCURRED')
+        #sendData(client, DISMES)
         client.close()
 
 
@@ -49,7 +51,7 @@ def start():
 ## CAM SET - Intakes Camera ID and Tuples rez (y,x), returns camera for cv2
 def cam_set(camID, rez, client):
     print(f'[CLIENT] Linking Camera #{camID}')
-    sendData(client, (f'Linking Camera #{camID}'))
+    #sendData(client, (f'Linking Camera #{camID}'))
     camera = cv2.VideoCapture(camID)
     camera.set(cv2.CAP_PROP_FRAME_HEIGHT, rez[0])
     camera.set(cv2.CAP_PROP_FRAME_WIDTH, rez[1])
@@ -65,27 +67,76 @@ def video_send(camera , client):
         if img == True:
             frame = cv2.imencode('.jpg', frame, ENCODEPARAM)[1].tobytes()
             #frame =  frame.tobytes()
-            sendData(client, frame)           
+            split_data(client, frame, SPLIT_RATE)           
 
 
 ##-----------------------------------------------------------------------------------------#
 ## SEND - Intakes data and sends to server
-def sendData(client , msg):
-    msg = pickle.dumps(msg)
-    print(len(msg))
-    msg_one = msg[:int(len(msg)/2)]
-    msg_two = msg[int(len(msg)/2):]
-    msg_one_len = str(len(msg_one)).encode(FORMAT)
-    msg_two_len = str(len(msg_two)).encode(FORMAT)
+def split_data(client:socket.socket, msg:bytes, split_rate:int)-> None:
+    """
+    Parameters: Client, message, and split rate\n
+    Function: Splits the messages into 'split rate' number of messages and message lengths\n
+    Affects: Affects the client by internalling calling send_data() method\n
+    Returns: Nothing
+    """
+    main_message = pickle.dumps(msg)
+    main_message_length = len(main_message)
+    
+    msg_length = int(main_message_length/split_rate)
+
+    msg_list = list()
+    msg_len_list = list()
+
+    i = 1
+
+    while i <= split_rate:
+        lower_bound = (i - 1) * msg_length
+        upper_bound = i * msg_length
+
+        temp_msg = main_message[lower_bound:upper_bound]
+        temp_len = str(len(temp_msg)).encode(FORMAT)
+        temp_len += b' ' * (HEADER - len(temp_len))
+
+        msg_list.append(temp_msg)
+        msg_len_list.append(temp_len)
+        i+=1
+
+    send_data(client,msg_list,msg_len_list)
+    #print(len(msg))
+    #msg_one = msg[:int(len(msg)/2)]
+    #msg_two = msg[int(len(msg)/2):]
+    #msg_one_len = str(len(msg_one)).encode(FORMAT)
+    #msg_two_len = str(len(msg_two)).encode(FORMAT)
     #msg_len = str(len(msg)).encode(FORMAT)
     #msg_len += b' ' * (HEADER - len(msg_len))
-    msg_one_len += b' ' * (HEADER - len(msg_one_len))
-    msg_two_len += b' ' * (HEADER - len(msg_two_len))
-    client.send(msg_one_len)
-    client.send(msg_one)
-    client.send(msg_two_len)
-    client.send(msg_two)
+    #msg_one_len += b' ' * (HEADER - len(msg_one_len))
+    #msg_two_len += b' ' * (HEADER - len(msg_two_len))
+    #client.send(msg_one_len)
+    #client.send(msg_one)
+    #client.send(msg_two_len)
+    #client.send(msg_two)
 
+
+
+def send_data(client:socket.socket,msg_list:list,msg_len_list:list):
+    """
+    Recieves the client, the split messages, and the split messages' lengths\n
+    First send the split rate to the server\n
+    Then sends each len and message tuple to the server\n
+    """
+    i = 0
+
+    split_rate_msg = str(len(msg_list)).encode(FORMAT)
+    split_rate_msg_len = str(len(split_rate_msg)).encode(FORMAT)
+    split_rate_msg_len += b' ' * (HEADER - len(split_rate_msg_len))
+
+    client.send(split_rate_msg_len)
+    client.send(split_rate_msg)
+
+    while i < len(msg_list):
+        client.send(msg_len_list[i])
+        client.send(msg_list[i])
+        i+=1
 
 
 start()
--- camera_networking/mainCameraServer.py
+++ camera_networking/mainCameraServer.py
@@ -37,50 +37,57 @@ def main_run(host):
         thread.start() # Puts each client on own thread
         print(f'[SERVER] NEW CONNECTION : {threading.active_count()-1} ACTIVE')
 
+
+def get_message(connection:socket.socket,split_rate:int)->bytes:
+    """
+    Parameters: Connection socket and split rate\n
+    Function: Calls socket.socket.recv() 'split rate' number of times\n\tand aggregates the message\n
+    Affects:Nothing\n
+    Returns: Returns the message in bytes
+    """
+
+    returnMessage = b''
+
+    i = 0
+    while i < split_rate:
+        msg_length = connection.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+
+        if msg_length:
+            msg = b''
+
+            while len(msg) < msg_length:
+                msg += connection.recv(msg_length - len(msg))
+            
+            returnMessage += msg
+            i+=1
+
+    return returnMessage
+
+
 ##-----------------------------------------------------------------------------------------#
 ## HANDEL CLIENT - Intakes connection and its adrress
-def handle_client(conn , addr):
+def handle_client(conn:socket.socket , addr):
     print(f'[SERVER] NEW CONNECTION AT {addr}')
     connected = True ## False to end conn
 
     while connected:
-        msg_one_len = conn.recv(HEADER).decode(FORMAT) ## Checks Header for message length
-        if msg_one_len: ## Check for no data
-            msg_one_len = int(msg_one_len) 
-            msg_one = b'' ## msg is the actual data being transfered
-            while len(msg_one) < msg_one_len: ## Collects all data to msg
-                msg_temp = conn.recv(msg_one_len-len(msg_one)) ## ensures all data collected
-                msg_one += msg_temp
+        split_msg_length = conn.recv(HEADER).decode(FORMAT)
+        split_msg = conn.recv(int(split_msg_length)).decode(FORMAT)
+        split_msg = int(split_msg)
 
-        msg_two_len = conn.recv(HEADER).decode(FORMAT)
-
-        if msg_two_len:
-            msg_two_len = int(msg_two_len) 
-            msg_two = b'' ## msg is the actual data being transfered
-            while len(msg_two) < msg_two_len: ## Collects all data to msg
-                msg_temp = conn.recv(msg_two_len-len(msg_two)) ## ensures all data collected
-                msg_two += msg_temp  
-
-        if msg_one_len and msg_two_len:
-            msg = msg_one + msg_two          
-            msg = pickle.loads(msg) ## Collected Message Unloaded
+        if split_msg:
+            msg = get_message(conn,split_msg)
+            msg = pickle.loads(msg)
             msg_type = type(msg)
-            
-            print(type(msg))
-            
-
-            if msg_type ==  str : ## if string show in console, or Disconnecting
+            if msg_type ==  str :
                 if msg == DISMES:
                     print(f'[CLIENT {addr}] DISCONNECTING')
                     connected = False
-                else: 
+                else:
                     print(f'[CLIENT {addr}] {msg}')
-
-            #elif msg_type == bytes: ## if list turn into and show image
-            else:    
-                #print("made it here")
-
-                msg = np.frombuffer(msg,np.byte) ## Byte Repair
+            elif msg_type == bytes:
+                msg = np.frombuffer(msg,np.byte)
                 msg = cv2.imdecode(msg, 1)
                 aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_100)
                 parameters = aruco.DetectorParameters()
@@ -88,16 +95,70 @@ def handle_client(conn , addr):
                 corners, ids, rejectedImgPoints = detector.detectMarkers(image=msg)
 
                 if type(ids) == type(None):
-                    cv2.imshow("RECIVEDVIDEO", msg)
+                   cv2.imshow("RECIVEDVIDEO", msg)
                 else:
-                    for id in ids:
+                   for id in ids:
                         print("[DETECED MARKER]: {}\n".format(id))
-                    editedFrame = aruco.drawDetectedMarkers(image=msg.copy(),corners=corners,ids=ids)
-                    cv2.imshow("DETECTED IMAGE",editedFrame)
+                        editedFrame = aruco.drawDetectedMarkers(image=msg.copy(),corners=corners,ids=ids)
+                        cv2.imshow("DETECTED IMAGE",editedFrame)
 
                 key = cv2.waitKey(1) & 0xFF
                 if key == ord('q'): ## if q is pressed disconnect
                     connected = False
+        #msg_one_len = conn.recv(HEADER).decode(FORMAT) ## Checks Header for message length
+        #if msg_one_len: ## Check for no data
+         #   msg_one_len = int(msg_one_len) 
+          #  msg_one = b'' ## msg is the actual data being transfered
+           # while len(msg_one) < msg_one_len: ## Collects all data to msg
+            #    msg_temp = conn.recv(msg_one_len-len(msg_one)) ## ensures all data collected
+             #   msg_one += msg_temp
+
+       # msg_two_len = conn.recv(HEADER).decode(FORMAT)
+
+        #if msg_two_len:
+         #   msg_two_len = int(msg_two_len) 
+          #  msg_two = b'' ## msg is the actual data being transfered
+           # while len(msg_two) < msg_two_len: ## Collects all data to msg
+            #    msg_temp = conn.recv(msg_two_len-len(msg_two)) ## ensures all data collected
+             #   msg_two += msg_temp  
+
+      #  if msg_one_len and msg_two_len:
+       #     msg = msg_one + msg_two          
+        #    msg = pickle.loads(msg) ## Collected Message Unloaded
+         #   msg_type = type(msg)
+            
+          #  print(type(msg))
+            
+
+           # if msg_type ==  str : ## if string show in console, or Disconnecting
+            #    if msg == DISMES:
+             #       print(f'[CLIENT {addr}] DISCONNECTING')
+              #      connected = False
+               # else: 
+                #    print(f'[CLIENT {addr}] {msg}')
+
+            #elif msg_type == bytes: ## if list turn into and show image
+            #else:    
+                #print("made it here")
+
+                #msg = np.frombuffer(msg,np.byte) ## Byte Repair
+                #msg = cv2.imdecode(msg, 1)
+                #aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_100)
+                #parameters = aruco.DetectorParameters()
+                #detector = aruco.ArucoDetector(aruco_dict,parameters)
+                #corners, ids, rejectedImgPoints = detector.detectMarkers(image=msg)
+
+                #if type(ids) == type(None):
+                   #cv2.imshow("RECIVEDVIDEO", msg)
+                #else:
+                   #for id in ids:
+                        #print("[DETECED MARKER]: {}\n".format(id))
+                        #editedFrame = aruco.drawDetectedMarkers(image=msg.copy(),corners=corners,ids=ids)
+                        #cv2.imshow("DETECTED IMAGE",editedFrame)
+
+                #key = cv2.waitKey(1) & 0xFF
+                #if key == ord('q'): ## if q is pressed disconnect
+                    #connected = False
                 
     conn.close()
     cv2.destroyAllWindows()
