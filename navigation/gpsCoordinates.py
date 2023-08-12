7b741fa2c2ea87dbac3ca5596ba902ae5d94179d
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2023-08-11 19:37:10 -0600

    changes

--- navigation/gpsCoordinates.py
+++ navigation/gpsCoordinates.py
@@ -1,14 +1,47 @@
 import serial
+import socket
+
+PORT = 9990
+SERVER = "192.168.1.3"
+ADDR = (SERVER,PORT)
+HEADER = 64
 
 command = serial.Serial('/dev/ttyUSB0',4800, timeout=5 )
 
+client = socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM)
+client.connect(ADDR)
+print("[CONNECTED TO SERVER AT {}]\n".format(PORT))
+
+def get_standard_coordinate(coordinate:str,direction:str)->str:
+    dd = float(coordinate[:2])
+    mm = float(coordinate[2:])
+
+    decimal_minutes = mm/60
+
+    decimate_coordinate = dd + decimal_minutes
+
+    if direction == "S" or direction == "W":
+        decimate_coordinate *= -1
+
+    return decimate_coordinate
+
 while True:
-    input = ser.readLine()
+    input = command.readLine()
     splitInputs = input.split(",")
 
     if splitInputs[0] == "$GPGGA":
         lat = splitInputs[2]
         lat_direction = splitInputs[3]
-        long = splitInputs[4]
+        longitude = splitInputs[4]
         long_direction = splitInputs[5]
-        print("Latitude: {}{}, Longitude: {}{}".format(lat,lat_direction,long,long_direction))
+        altitude = splitInputs[9]
+
+        lat = get_standard_coordinate(lat,lat_direction)
+        longitude = get_standard_coordinate(longitude,long_direction)
+        message = "({},{},{})".format(lat,longitude,altitude)
+        message = message.encode('utf-8')
+        message_length = str(len(message)).encode('utf-8')
+        message_length += b' ' * (HEADER - len(message_length))
+
+        client.send(message_length)
+        client.send(message)
